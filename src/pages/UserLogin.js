c385ff0e43ed171b3e5773eceebde9f94d2d168a
MeriemSellami <sellamimeriem2003@gmail.com>
2024-12-20 17:19:50 +0100

    captcha removed

--- src/pages/UserLogin.js
+++ src/pages/UserLogin.js
@@ -1,34 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import ReCAPTCHA from "react-google-recaptcha"; // Import the reCAPTCHA component
+
 
 const UserLogin = () => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
-  const [captchaValue, setCaptchaValue] = useState(null); // To store the reCAPTCHA response
+
   const navigate = useNavigate();
 
-  // Handle captcha change
-  const handleCaptchaChange = (value) => {
-    setCaptchaValue(value);
-  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
-    // Check if captcha is completed
-    if (!captchaValue) {
-      setError("Please complete the CAPTCHA.");
-      return;
-    }
 
     try {
       console.log("Logging in with credentials:", credentials);
 
       const response = await axios.post("http://localhost:5000/api/users/login", {
-        ...credentials,
-        captcha: captchaValue, // Add captcha response to the request
+        ...credentials
       });
       console.log("Response:", response.data);
 
@@ -68,11 +57,6 @@ const UserLogin = () => {
           value={credentials.password}
           onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
           style={{ display: "block", width: "100%", marginBottom: "10px" }}
-        />
-        {/* Add the reCAPTCHA widget */}
-        <ReCAPTCHA
-          sitekey="6Ld97pgqAAAAAClaCyScyQcJ8W7sr8e0jjacgsR4" // Replace with your site key from Google reCAPTCHA
-          onChange={handleCaptchaChange}
         />
         <button type="submit" style={{ width: "100%" }}>Login</button>
       </form>
